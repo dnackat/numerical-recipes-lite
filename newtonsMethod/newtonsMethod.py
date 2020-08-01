2805e3c2641d6dc6004dd33cc73afed03f202fb8
Dileep Nackathaya <dileepbn@gmail.com>
2020-08-01 14:32:10 +0530

    coding newtons method

---
+++ newtonsMethod/newtonsMethod.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 14:25:47 2020
+
+@author: dileepn
+
+Newton's method: A toy example
+"""
+import numpy as np
+
+f = np.exp
+
+def fn(f,x):
+     """
+     
+     """
+     return float(map(f,x))
+
+def d_fn(x,h=1e-6):
+     
+
+     
