cbbf2d013d8d38c7842d52cbfa498f3ab5673e13
Dileep Nackathaya <dileepbn@gmail.com>
2020-08-01 14:41:54 +0530

    fun and derivative calc complete

--- newtonsMethod/newtonsMethod.py
+++ newtonsMethod/newtonsMethod.py
@@ -13,11 +13,29 @@ f = np.exp
 
 def fn(f,x):
      """
+     Inputs:
+          - f: function definition (e.g. f = exp)
+          - x: initial guess or subsequent predicitons
      
+     Output:
+          - Function value at x i.e. f(x)
      """
+     
      return float(map(f,x))
 
 def d_fn(x,h=1e-6):
+     """
+     Inputs:
+          - x: initial guess or subsequent predicitons
+          
+     Outputs:
+          - Derivative of f at x i.e. df(x)/dx
      
-
+     Note: Derivative is computed numerically using the forward difference method
+           df(x)/dx = (f(x+h)-f(x))/h
+           To change the interval, h, pass it as the second argument to the function
+           (e.g. d_fn(x,1e-4))
+     """
+     
+     
      
