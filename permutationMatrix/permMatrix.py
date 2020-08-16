8b8731e3651f818d0a596d08fbd88098e884968a
Dileep Nackathaya <dileepbn@gmail.com>
2020-08-16 15:11:20 +0530

    perm matrix row and col exchange part..

--- permutationMatrix/permMatrix.py
+++ permutationMatrix/permMatrix.py
@@ -10,11 +10,16 @@ This script creates permutation matrices of specified sizes for row or column ex
 
 import numpy as np
 
-def permMatrix(dim=3,ex='r',order=(1,2,3,4)):
+def permMatrix(dim=3,ex='r',order=[0]):
      """
      Inputs:
           dim (integer): The size of the permutation matrix (square, so 3 means a 3x3 matrix)
           ex (r or c): r means row exchanges; c means column exchanges
+          order (list of integers): The order in which to shuffle rows or columns.
+                                   e.g. 4321 means 4th row is switched with 1st row,
+                                        3rd row is switched with the 2nd row, etc.
+                                   Note that the size of the list must be equal to 'dim'.
+          
           
      Output:
           The desired permutation matrix
@@ -40,4 +45,17 @@ def permMatrix(dim=3,ex='r',order=(1,2,3,4)):
           print("\nInvalid entry for exchange paramter. Check function help and try again.\n")
           return
      
-     # Create 
+     # Create an array to hold the permutation matrix
+     P = np.identity(dim)
+     
+     # Build the desired permutation matrix
+     if len(order) == 1 and order[0] == 0:
+          # Do nothing. Just return the identity matrix
+          return P
+     elif len(order) > 1:
+          # Iterate through 'order' list and only populate rows/cols that need to be exchanged
+          for i in range(len(order)):
+               if not (order[i] - 1 == i):
+                    if ex == 'r':
+                    # Row exchanges
+                         P[i,:] = np.identity(dim)[order[i]-1,:]
