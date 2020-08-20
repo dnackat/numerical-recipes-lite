39e56e86bec13379fdcc45bb2e8352c78c7b5eda
Dileep Nackathaya <dileepbn@gmail.com>
2020-08-20 20:43:47 +0530

    playing around with calc: curves with tangents that satisfy a condition

--- Misc/vec_Fields.py
+++ Misc/vec_Fields.py
@@ -21,3 +21,29 @@ v = y/np.sqrt(x**2 + y**2)
 
 plt.quiver(x,y,u,v,color='rgb')
 plt.grid(axis='both')
+
+#%% Plane curve where every point bisects the part of the tangent in the first quadrant passing through that point
+c = 1
+
+x = np.linspace(0.1,5,100)
+
+# Curve that satisfies the condition
+y = c/x
+
+# Point for checking lengths
+x1 = 1.25
+y1 = c/x1
+
+# Points of intersection of tnagnet line with x and y-axes
+a = 2*x1
+b = 2*y1
+
+# Calculate lengths
+len1 = np.sqrt(x1**2 + (b-y1)**2)
+len2 = np.sqrt((a-x1)**2 + y1**2)
+
+if abs(len1-len2) < 1e-3:
+     print("Close enough. P is a bisector of the tangent line.")
+else:
+     print("Nope. P is not the bisector of the tangent line.")
+
