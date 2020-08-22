91158568181cec9040fc77f4d9c430e6e0bf63bb
Dileep Nackathaya <dileepbn@gmail.com>
2020-08-22 22:46:52 +0530

    playing around with harmonic functions..

--- Misc/vec_Fields.py
+++ Misc/vec_Fields.py
@@ -14,16 +14,28 @@ import matplotlib.pyplot as plt
 #%% Vector plots
 x,y = np.meshgrid(np.linspace(-2,2,10),np.linspace(-2,2,10))
 
-u = x/np.sqrt(x**2 + y**2)
-v = y/np.sqrt(x**2 + y**2)
+u = -y/np.sqrt(x**2 + y**2)
+v = x/np.sqrt(x**2 + y**2)
 
 #z = 1/(2*np.pi)*np.exp(-(x**2 + y**2)/2)
 
-plt.quiver(x,y,u,v,color='rgb')
+plt.quiver(x,y,u,v,color=['red','green','blue'])
 plt.grid(axis='both')
 
+#%% Harmonic functions
+x,y = np.meshgrid(np.linspace(-5,5,100),np.linspace(-5,5,100))
+
+a = -1.25
+b = 2.75
+k = 2.5
+
+z = a*(x**2 - y**2) + b*x*y
+z1 = np.exp(k*x)*np.sin(k*y)  
+
+plt.contourf(x,y,z,cmap='plasma')
+
 #%% Plane curve where every point bisects the part of the tangent in the first quadrant passing through that point
-c = 1
+c = 2
 
 x = np.linspace(0.1,5,100)
 
