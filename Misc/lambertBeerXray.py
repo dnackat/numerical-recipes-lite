017ac884da88875ede7e28477e0cfcb6e18c7af7
Dileep Nackathaya <dileepbn@gmail.com>
2021-07-14 19:53:39 +0530

    lambert-beer model for X-ray attenuation

---
+++ Misc/lambertBeerXray.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 14 19:43:21 2021
+
+@author: dileepn
+
+Lambert-Beer model for X-ray attenuation
+"""
+# Preliminaries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Intial intensity 
+I_0 = 1.0
+
+# Attenuation coefficient
+mu = 1.0
+
+# length of passage
+dx = 1.0
+
+# Combinations to try
+data = [(0.5,1),(0.5,2),(1,2),(2,0.5),(3,3)]
+
+for entry in data:
+     # Unpack tuple
+     mu, dx = entry
+     
+     
+     
