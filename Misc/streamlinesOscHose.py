4cd1e22655eaa606c2cb5062fec2e8d49e1a6311
Dileep Nackathaya <dileepbn@gmail.com>
2021-07-24 00:08:51 +0530

    code to generate streamline plot for oscillating hose

--- Misc/streamlinesOscHose.py
+++ Misc/streamlinesOscHose.py
@@ -16,11 +16,26 @@ import matplotlib.pyplot as plt
 # Constants
 u_0 = 1. # m/s
 v_0 = 2. # m/s
-omega = 2 # oscillations per second
+omega = 2. # oscillations per second
 
 
 # Create a 1D grid
 y = np.linspace(-5,5,100)
 
 # Streamline function for t = 0 and (x,y) = (0,0)
-x = 
+x1 = (u_0/omega)*(np.cos(omega*y/v_0)-1.)
+
+# Streamline function for t = pi/(2*omega) and (x,y) = (0,0)
+x2 = (u_0/omega)*(np.sin(omega*y/v_0))
+
+# Create plot
+plt.figure()
+#plt.rc('text', usetex=True)
+#plt.rc('font', family='serif')
+plt.plot(y,x1,'r',linewidth=2,label="r'$t = 0$'")
+plt.plot(y,x2,'b',linewidth=2,label="r't = \frac{$\pi$}{$2*\omega$}'")
+plt.xlabel("r'$x_s$'")
+plt.ylabel("r'$y_s$'")
+plt.legend(loc="best")
+plt.grid()
+plt.show()
