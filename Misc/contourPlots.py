a4c707646ea15f584403829ae7a619e02c235bce
Dileep Nackathaya <dileepbn@gmail.com>
2021-07-25 19:57:12 +0530

    contour plots of fns of 2 variables: in progress

--- Misc/contourPlots.py
+++ Misc/contourPlots.py
@@ -19,4 +19,18 @@ y = np.linspace(-3,3,60)
 X, Y = np.meshgrid(x,y)
 
 # Function definitions
- 
+def f1(x,y):
+     return 2*x + y
+
+def f2(x,y):
+     return x**2 + y**2 
+
+def f3(x,y):
+     return x**2 - y**2
+
+# Calculate function values
+Z1 = f1(X,Y)
+Z2 = f2(X,Y)
+Z3 = f3(X,Y)
+
+# Create plots
