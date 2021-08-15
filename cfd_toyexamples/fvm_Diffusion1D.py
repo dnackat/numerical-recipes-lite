cdd0f4729e266b8c09abc07db7a5104f948729a1
Dileep Nackathaya <dileepbn@gmail.com>
2021-08-15 21:21:21 +0530

    grid created, sources and boundary conditions specified

--- cfd_toyexamples/fvm_Diffusion1D.py
+++ cfd_toyexamples/fvm_Diffusion1D.py
@@ -16,12 +16,20 @@ import matplotlib.pyplot as plt
 # Parameters
 Gamma = 1.0    # Diffusivity
 
-
 # Create the grid
 fc = np.arange(0.,6.,1)  # Face centroids
 cc = (fc + (fc + 1.))/2.      # Cell centroids
 
+# Cell width
+dx = fc[1] - fc[0]
+
+# Source terms
+S = 6.*cc*dx      # Linear source term, S*d(vol) = 6*x*(dx*1.0)
+
 # Vector of unknowns
-phi = np.ones((len(cc),1))
+phi = np.ones((len(cc)+2,1)) # Length should be cell centroids + no. of boundary faces
       
+# Boundary conditions
+phi[0] = 10.
+phi[len(phi)-1] = 135.
 
