c834a2b0ee1bf042cdd2bad0397f2d297ebacb7c
Dileep Nackathaya <dileepbn@gmail.com>
2021-08-27 15:38:06 +0530

    made significant changed to code to handle more complex cases and improved plotting

--- Misc/constrainedOptimization.py
+++ Misc/constrainedOptimization.py
@@ -16,29 +16,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Grid
-x = np.arange(0, 2.5, 0.25)
-y = np.arange(0, 2.5, 0.25)
+x = np.arange(-7.07, 7.07, 0.125)
+y = np.arange(-7.07, 7.07, 0.125)
 
 xx, yy = np.meshgrid(x, y)
 
 # Objective function: perimeter of the inscribed rectangle
-f = 4*xx + 4*yy 
+f = xx**2 - 6*xx + yy**2 - 6*yy + xx*yy 
 
-# Constraint curve: a circle with radius 2 centered around the origin
-g = xx*xx + 4*yy*yy
+# Constraint curve: a circle with radius 5 centered around the origin
+g = xx*xx + yy*yy
 
 # Coloring
 color = g
 
 # Gradients
-fx, fy = np.gradient(f, 0.25, 0.25)
-gx, gy = np.gradient(g, 0.25, 0.25)
+fx, fy = np.gradient(f, 0.5, 0.5)
+gx, gy = np.gradient(g, 0.5, 0.5)
 
 # Plot
-cx = np.linspace(0,2,100)
+
+# Gradeint and constraint
+cx = np.linspace(-7.07,7.07,100)
 plt.figure(figsize=(16,16)) 
-plt.plot(cx,np.sqrt(1-cx**2/4.),linewidth=2.5)
-plt.quiver(xx, yy, fx, fy, color)
-plt.quiver(xx, yy, gx, gy)
+plt.plot(cx,np.sqrt(50-cx**2),'r',linewidth=2.5)
+plt.plot(cx,-np.sqrt(50-cx**2),'r',linewidth=2.5)
+#plt.quiver(xx, yy, fx, fy, color)
+#plt.quiver(xx, yy, gx, gy)
 plt.xlabel(r'$x$',fontsize=20)
 plt.ylabel(r'$y$',fontsize=20)
+
+# Level curves
+level_curves = [-10, -6, 0, 10, 30, 40, 50, 60, 75,100]
+#c = plt.pcolormesh(xx, yy, f, cmap='nipy_spectral', vmin=f.min(), vmax=f.max(), shading='auto')
+plt.axis([xx.min(), xx.max(), yy.min(), yy.max()])
+CS = plt.contour(xx, yy, f, level_curves, colors='black', linewidths=0.75)
+plt.clabel(CS, inline=True, fmt='%1.0f', fontsize=16)
+#plt.colorbar(c)
+plt.show()
