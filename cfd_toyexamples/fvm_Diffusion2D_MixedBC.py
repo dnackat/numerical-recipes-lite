2e626c2db2255504bd3a918cc4e6da4becd05233
Dileep Nackathaya <dileepbn@gmail.com>
2021-08-31 23:58:56 +0530

    preliminaries done for 2d diffusion equation by FVM..

--- cfd_toyexamples/fvm_Diffusion2D_MixedBC.py
+++ cfd_toyexamples/fvm_Diffusion2D_MixedBC.py
@@ -6,10 +6,12 @@ Created on Tue Aug 31 21:03:25 2021
 @author: dileepn
 
 This is a toy example of using the Finite-Volume method to solve the 2-D 
-heat diffusion problem with mixed boundary conditons. The physics is governed 
-by the equation:
+heat diffusion problem with mixed boundary conditons (Dirichlet, von Neumann,
+and a combination of the two). 
+The physics is governed by the equation:
      div(rho*u*T) = div(k*grad(T)) + source term
 """
+
 # Preliminaries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,12 +24,14 @@ domain_length = 1.0 # In both x and y directions in m
 
 # Create the grid
 numcells = 5   # Number of cells in the x and y directions
-fc_x = np.linspace(0.,domain_length,numcells)  # x face centroids
-fc_y = np.linspace(0.,domain_length,numcells)  # y face centroids
-fxx, fyy = np.meshgrid(fc_x,fc_y)       # Create a 2D grid with face centroids
+fc_x = np.linspace(0.,domain_length,numcells+1)  # x face centroids
+fc_y = np.linspace(0.,domain_length,numcells+1)  # y face centroids
+fxx = np.tile(fc_x,numcells+1)       # Long vector of x face centroids
+fyy = np.repeat(fc_y,numcells+1)       # Long vector of y face centroids
 cc_x = (fc_x[:-1] + (fc_x[1:]))/2.      # x cell centroids
 cc_y = (fc_y[:-1] + (fc_y[1:]))/2.      # y cell centroids
-cxx, cyy = np.meshgrid(cc_x,cc_y)       # Create a 2D grid with cell centroids
+cxx = np.tile(cc_x,numcells)       # Long vector of x cell centroids
+cyy = np.repeat(cc_y,numcells)       # Long vector of y cell centroids
 
 # Calculate all the cell distances for convenience as they remain constant in this problem
 delta_x = fc_x[2] - fc_x[1] # Distance between faces in the x direction
@@ -44,27 +48,30 @@ area_y = delta_x*thickness # Areas of faces with normal vector aligned with the 
 cell_volume = delta_x*delta_y*thickness
 
 #%%
-# Source terms
-S = 100.*cc*dx      # Linear source term, S*d(vol) = 6*x_centroid*(dx*1.0)
+# Volumetric source. Depends on cell centroid values
+S = 100.*(1. + 5*cxx + 5*cyy)*cell_volume      # Linear source term, S*d(vol) = 6*x_centroid*(dx*1.0)
 
-# Vector of unknowns
-phi = np.ones((len(cc),1)) # Length should be equal to no. of cell centroids; 
+ #%%
+# Vector of unknown temperatures
+T = np.ones((len(cc_x),1)) # Length should be equal to no. of cell centroids; 
                            # boundary values lumped with constants in b vector
                               
-# Boundary conditions for phi (included with constants in b vector)
-phi_first = 10.     # phi on first boundary face
-phi_last = 135.     # phi on last boundary face
+# Boundary conditions for T (included with constants in b vector)
+T_right = 500.    # T in K on right boundary face
+T_left = 500.     # T in K on left boundary face
+T_top = 500.      # T in K on top boundary face
+T_inf = 300.      # T in K of flow across bottom face
 
-# Vector of constants
-b = np.zeros((len(cc),1)) # Length = no. of cell centroids
+# Vector of constants.
+b = np.zeros((len(cc_x),1)) # Length = no. of cell centroids
 
 # Populate the vector of constants
 b = S.reshape((len(S),1))     # Source term contribution
-b[0] += (k/(cc[0]-fc[0]))*phi_first    # Add the boundary value for first face
-b[-1] += (k/(fc[-1]-cc[-1]))*phi_last  #  Add the boundary value for last face
+b[0] += (k/(cc[0]-fc[0]))*T_first    # Add the boundary value for first face
+b[-1] += (k/(fc[-1]-cc[-1]))*T_last  #  Add the boundary value for last face
       
-# Matrix of coefficients (size = size(b)*size(phi))
-A = np.zeros((len(b),len(phi)))
+# Matrix of coefficients (size = size(b)*size(T))
+A = np.zeros((len(b),len(T)))
 
 # Populate A. It is sparse and diagonally dominant, so a single loop should suffice
 for i in range(len(b)): 
@@ -100,37 +107,37 @@ def gauss(A, b, x, n):
      return x
 
 # Solve the system 
-phi_fvm = gauss(A, b, phi, 100)
+T_fvm = gauss(A, b, T, 100)
 
 # Analytical (exact) solution
 x = np.linspace(0.,5.,100)
-phi_exact = 10 + 50*x - x**3
+T_exact = 10 + 50*x - x**3
 
 # Print the result
 print("\n")
 print("---------- Solution ----------")
 print("\n")
-print("PHI values from FVM method:\n")
-print(phi_fvm.round(2))
+print("T values from FVM method:\n")
+print(T_fvm.round(2))
 
 # Plot the FVM and exact solution
 cc_plot = np.zeros(len(cc)+2)   # Add boundary points just for plotting
 cc_plot[-1] = fc[len(cc)]
 cc_plot[1:-1] = cc
-phi_plot = np.zeros(len(cc)+2)  # Add boundary points just for plotting
-phi_plot[0] = phi_first
-phi_plot[-1] = phi_last
-phi_plot[1:-1] = phi_fvm.reshape(len(phi_fvm))
+T_plot = np.zeros(len(cc)+2)  # Add boundary points just for plotting
+T_plot[0] = T_first
+T_plot[-1] = T_last
+T_plot[1:-1] = T_fvm.reshape(len(T_fvm))
 
 plt.figure(figsize=(16,16))
-plt.plot(x,phi_exact,'k',linewidth=1,label="Exact solution") # Plot of the exact solution
-plt.scatter(cc_plot,phi_plot,c="red",marker="o",s=200,label="FVM soluton") # Scatter plot of FVM resluts
+plt.plot(x,T_exact,'k',linewidth=1,label="Exact solution") # Plot of the exact solution
+plt.scatter(cc_plot,T_plot,c="red",marker="o",s=200,label="FVM soluton") # Scatter plot of FVM resluts
 for c in cc:
      plt.axvline(x=c,color='b',linestyle='--',linewidth=0.5)    # Vertical lines from centroid for clarity
 plt.grid(axis="both")
 plt.xlim((0.,5.))
 plt.xlabel(r'$x$',fontsize=16)
-plt.ylabel(r'$\phi$',fontsize=16)
+plt.ylabel(r'$\T$',fontsize=16)
 plt.legend(loc="best",fontsize=16)
 plt.title("Comparison between FVM and exact solution for the 1D Diffusion Equation"\
           ,fontsize=20) 
