b841f7b184c8d037a2a2035b40cd48b748b47cb2
Dileep Nackathaya <dileepbn@gmail.com>
2021-09-30 23:26:38 +0530

    hagen poiseuille flow of oil in a pipeline: plotting profile

--- Misc/hagenPoiseuille.py
+++ Misc/hagenPoiseuille.py
@@ -7,3 +7,27 @@ Created on Thu Sep 30 20:43:21 2021
 
 Simplified planar case of Hagen-Poiseuille flow of oil in a lubricated pipe. 
 """
+
+# Preliminaries
+import numpy as np
+import matplotlib.pyplot as plt
+
+#### Non-dimensionalized velocity profile in oil ####
+y_by_H = np.linspace(0.,1.,100) # Non-dimensional vertical distance
+H1_by_H = 0.8 # Ratio of oil height to pipe height
+
+# Viscosity ratio of oil to water in the pipe
+visc_ratio1 = 2
+visc_ratio2 = 3
+visc_ratio3 = 10
+visc_ratio4 = 100
+
+V1 = (1 - H1_by_H**2) + visc_ratio1*(H1_by_H**2 - y_by_H**2)
+V2 = (1 - H1_by_H**2) + visc_ratio2*(H1_by_H**2 - y_by_H**2)
+V3 = (1 - H1_by_H**2) + visc_ratio3*(H1_by_H**2 - y_by_H**2)
+V4 = (1 - H1_by_H**2) + visc_ratio4*(H1_by_H**2 - y_by_H**2)
+
+## Plot the velocity profile ##
+
+
+
