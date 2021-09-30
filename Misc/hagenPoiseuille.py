819cd2242b2439ad87dfa95f392d76f1f202ac2e
Dileep Nackathaya <dileepbn@gmail.com>
2021-09-30 20:45:25 +0530

    hagen poiseuille flow of oil in a pipeline..

---
+++ Misc/hagenPoiseuille.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 20:43:21 2021
+
+@author: dileepn
+
+Simplified planar case of Hagen-Poiseuille flow of oil in a lubricated pipe. 
+"""
