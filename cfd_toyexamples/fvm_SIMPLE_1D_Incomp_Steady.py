3c5e992e4dca256ff8f45c412f3ff23bc9f87532
Dileep Nackathaya <dileepbn@gmail.com>
2021-10-13 00:39:03 +0530

    solution for 1d SIMPLE scheme on a staggered grid: done but system is singular..

--- cfd_toyexamples/fvm_SIMPLE_1D_Incomp_Steady.py
+++ cfd_toyexamples/fvm_SIMPLE_1D_Incomp_Steady.py
@@ -37,7 +37,7 @@ alphaU = 1     # URF for x-velocity (no relaxation if set equal to 1)
 alphaP = 1     # URF for pressure (no relaxation if set equal to 1)
 
 # Maximum iterations permitted
-maxiter = 100 
+maxiter = 100
 
 # Begin the main loop 
 for i in range(maxiter):
@@ -59,7 +59,7 @@ for i in range(maxiter):
      
      # Solve momentum equations (under-relaxed)
      uA = ((p1 - p2) + bA + uA*aA*(1.0 - alphaU)/alphaU)*alphaU/aA
-     uB = ((p2 - p3) + uB*aB*(1.0 - alphaU)/alphaU)*alphaU/aB
+     uB = ((p2 - p3) + bB + uB*aB*(1.0 - alphaU)/alphaU)*alphaU/aB
      
      # Calculate residual for the continuity equations
      c_residual = (abs(uA - 1) + abs(uB - uA) + abs(1 - uB))
@@ -69,7 +69,7 @@ for i in range(maxiter):
      if (u_residual + u_residual < tolerance):
           print("Converged solution is: p1 = {:.2f} \n \
                 p2 = {:.2f} \n p3 = {:.2f} \n uA = {:.2f} \n \
-                uB = {:.2f}\n").format(p1, p2, p3, uA, uB)
+                uB = {:.2f}\n".format(p1, p2, p3, uA, uB))
                 
      
      # Solve the linear system for pprimes 
@@ -83,7 +83,21 @@ for i in range(maxiter):
      p3prime = z.item()
      
      # Correct the velocities and pressures
+     uAprime = dA*(p1prime - p2prime)
+     uBprime = dB*(p2prime - p3prime)
+     uA = uA + uAprime
+     uB = uB + uBprime
+     p1 = p1 + alphaP*p1prime
+     p2 = p2 + alphaP*p2prime
+     p3 = p3 + alphaP*p3prime
      
+     # Check if continuity is satisfied 
+     cont1 = uA - 1.
+     cont2 = uB - uA
+     cont3 = 1. - uB
      
-     
-     
+     # Output 
+     if i == 0:
+          print("Iter \t uA \t uB \t p1 \t p2 \t p3 \t u_res \t cont_res \t cont1 \t cont2 \t cont3")
+          
+     print("{0:2d} {0:1.3e} {0:1.3e} {0:1.3e} {0:1.3e} {0:1.3e} {0:1.3e} {0:1.3e} {0:1.3e} {0:1.3e} {0:1.3e}".format(i, uA, uB, p1, p2, p3, u_residual, c_residual, cont1, cont2, cont3))  
