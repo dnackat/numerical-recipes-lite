5c07a120ca100f463024d6f50b6587faa1f3752e
Dileep Nackathaya <dileepbn@gmail.com>
2021-10-12 20:43:32 +0530

    coding solution for 1d, incomp, steady flow with SIMPLE scheme on a staggered grid..

---
+++ cfd_toyexamples/fvm_SIMPLE_1D_Incomp_Steady.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 20:40:19 2021
+
+@author: dileepn
+
+Solution of 1D, steady, incompressible flow on a staggered grid along with
+SIMPLE scheme for pressure-velocity coupling. 
+ 
+"""
